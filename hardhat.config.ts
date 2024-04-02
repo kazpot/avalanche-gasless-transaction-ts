ec62b87fb31051afd916ae485e4b5c575f121c4a
kaz <knman7@hotmail.com>
2024-04-02 12:19:38 +0900

    Update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,23 @@
-import * as dotenv from "dotenv"; // 環境構築時にこのパッケージはインストールしてあります。
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 
-// .envファイルから環境変数をロードします。
 dotenv.config();
 
-if (process.env.PRIVATE_KEY === undefined) {
-  console.log("private key is missing");
-}
-
 const CHAIN_ID = process.env.CHAIN_ID || "";
+const CHAIN_RPC_URL = process.env.CHAIN_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     subnet: {
-      url: process.env.CHAIN_RPC_URL,
+      url: CHAIN_RPC_URL,
       chainId: parseInt(CHAIN_ID),
       gasPrice: 700000000000,
-      accounts: [process.env.PRIVATE_KEY || ""],
+      accounts: [PRIVATE_KEY],
     },
   },
 };
